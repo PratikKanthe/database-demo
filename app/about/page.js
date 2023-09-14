83ece9f223ac013b9762db03ad1a495ec78e79dc
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-09-14 09:17:17 +0530

    added about page

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+function About() {
+  return (
+    <>
+        <div>About <Link href="/">us</Link></div>
+    </>
+  )
+}
+
+export default About
